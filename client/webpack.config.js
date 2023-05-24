d4889c616b0cf92feb6bf66e90b2c154000a8d00
sheaschwenn <sheaschwenn@gmail.com>
2023-05-24 15:10:00 -0700

    manifest.json fixed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,18 +23,37 @@ module.exports = () => {
         template: './index.html',
         title: 'Text Editor'
       }),
-      new MiniCssExtractPlugin(),
-      // new InjectManifest({
-      //   swSrc: './src/sw/js',
-      //   swDest: 'service-worker.js',
-      // })
-    ],
+      
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
+     
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Jate',
+      short_name: 'Jate',
+      description: 'Just another text editor',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('favicon.ico'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
